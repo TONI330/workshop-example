d1dc78f3fe5c5ca266b057150eb46d66f959de4c
Teun <t.j.tleenders@gmail.com>
2021-02-10 10:39:15 +0100

    added sum method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("Hello world");
+        System.out.println(sum(2, 4));
 
     }
+
+    public static double sum(int a, int b) {
+        return a*b;
+    }
+
+
+
 }
